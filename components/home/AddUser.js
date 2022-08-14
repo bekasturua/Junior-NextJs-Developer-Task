2a16c001ebb350be77c83e0260881bb3b0ef8e40
Beka Sturua <beqasturua3@gmail.com>
2022-08-14 19:09:23 +0400

    feat: add user addition

--- components/home/AddUser.js
+++ components/home/AddUser.js
@@ -1,32 +1,81 @@
 import classes from "./AddUser.module.css";
+import axios from "axios";
+import { useState } from "react";
 
 function AddUser() {
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    dateOfBirth: "",
+    statusId: 0,
+    roleId: 0,
+  });
+
+  const addSubmitHandler = async () => {
+    const res = await axios.post("http://localhost:3000/users", user);
+  };
+
   return (
     <div>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={addSubmitHandler}>
         <div className={classes.control}>
           <label>First Name</label>
-          <input type="text" required id="title" />
+          <input
+            type="text"
+            required
+            id="first_name"
+            onChange={(event) => {
+              setUser({ ...user, firstName: event.target.value });
+            }}
+          />
         </div>
         <div className={classes.control}>
-          <label>Second Name</label>
-          <input type="url" required id="image" />
+          <label>Last Name</label>
+          <input
+            type="text"
+            required
+            id="last_name"
+            onChange={(event) => {
+              setUser({ ...user, lastName: event.target.value });
+            }}
+          />
         </div>
         <div className={classes.control}>
-          <label htmlFor="address">Date Of Birth</label>
-          <input type="text" required id="address" />
+          <label>Date Of Birth</label>
+          <input
+            type="date"
+            required
+            id="date_of_birth"
+            onChange={(event) => {
+              setUser({ ...user, dateOfBirth: event.target.value });
+            }}
+          />
         </div>
         <div className={classes.control}>
-          <label htmlFor="description">Age</label>
-          <input type="text" required id="description" />
+          <label>Status</label>
+          <input
+            type="number"
+            required
+            id="status"
+            min={1}
+            max={2}
+            onChange={(event) => {
+              setUser({ ...user, statusId: Number(event.target.value) });
+            }}
+          />
         </div>
         <div className={classes.control}>
-          <label htmlFor="description">Status</label>
-          <input type="text" required id="description" />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="description">Role</label>
-          <input type="text" required id="description" />
+          <label>Role</label>
+          <input
+            type="number"
+            required
+            id="role"
+            min={1}
+            max={2}
+            onChange={(event) => {
+              setUser({ ...user, roleId: Number(event.target.value) });
+            }}
+          />
         </div>
         <div className={classes.actions}>
           <button>Add User</button>
